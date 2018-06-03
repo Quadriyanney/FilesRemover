84be065779160ba5d1b9c2b5c1065bd2708c2dd4
Anifowose <Quadriyanney@gmail.com>
2018-06-03 05:22:10 +0100

    Initial Commit

---
+++ app/src/main/java/com/quadriyanney/filesremover/MainActivity.java
@@ -0,0 +1,13 @@
+package com.quadriyanney.filesremover;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
